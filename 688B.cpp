a46d0d31290b4755b8d09c905d1db903f9689e40
zoo868e <zoo868e@gmail.com>
2021-07-22 15:19:07 +0800

    Accept 1000
    Because the problem is asking the n-th even-length palindrome number, so
    only have to print the input and the reverse of input.

---
+++ 688B.cpp
@@ -0,0 +1,18 @@
+#include"bits/stdc++.h"
+#define ll long long int
+
+using namespace std;
+
+void solve(){
+	string n;
+	cin >> n;
+	cout << n;
+	reverse(n.begin(), n.end());
+	cout << n << endl;
+}
+
+int main(){
+	solve();
+	return 0;
+}
+
